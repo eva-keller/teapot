3be4b9de073cf21568ca7a1690e46477ea3405d2
Eva Keller <eva.keller1@gmail.com>
2020-11-20 17:20:06 +0100

    notes on DOM

---
+++ dom_handling.js
@@ -0,0 +1,59 @@
+<div id="container">
+  <div class="display"></div>
+  <div class="controls"></div>
+</div>
+
+const container = document.querySelector('#container');
+// select the #container div (don't worry about the syntax, we'll get there)
+
+console.dir(container.firstElementChild);                      
+// select the first child of #container => .display
+
+const controls = document.querySelector('.controls');   
+// select the .controls div
+
+console.dir(controls.previousElementSibling);                  
+// selects the prior sibling => .display
+
+const div = document.createElement('div');                     
+// create a new div referenced in the variable 'div'
+
+div.style.color = 'blue';                                      
+// adds the indicated style rule
+
+div.style.cssText = 'color: blue; background: white';          
+// adds several style rules
+
+div.setAttribute('style', 'color: blue; background: white');    
+// adds several style rules
+
+div.style.background-color // doesn't work - attempts to subtract color from div.style.background
+div.style.backgroundColor // accesses the divs background-color style
+div.style['background-color'] // also works
+div.style.cssText = "background-color: white" // ok in a string
+
+div.setAttribute('id', 'theDiv');                              
+// if id exists update it to 'theDiv' else create an id
+// with value "theDiv"
+
+div.getAttribute('id');                                        
+// returns value of specified attribute, in this case
+// "theDiv"
+
+div.removeAttribute('id');                                     
+// removes specified attribute
+
+div.classList.add('new');                                      
+// adds class "new" to your new div
+
+div.classList.remove('new');                                   
+// remove "new" class from div
+
+div.classList.toggle('active');                                
+// if div doesn't have class "active" then add it, or if
+// it does, then remove it
+
+div.textContent = 'Hello World!'                               
+// creates a text node containing "Hello World!" and
+// inserts it in div
+
