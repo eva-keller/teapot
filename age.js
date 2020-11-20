b441b4c7d9a2ff669b8eaead0bdaa115c1b66c08
Eva Keller <eva.keller1@gmail.com>
2020-11-20 11:09:00 +0100

    age counter, factorial counter

--- age.js
+++ age.js
@@ -1,11 +1,15 @@
 //includes someone's age and then 
 //calculates and reports the future age in 10, 20, 30 and 40 years.
+//use a for loop to display the future ages
 let rlSync = require('readline-sync');
 let age = Number(rlSync.question("How old are you? \n"));
 
-
 console.log(`You are ${age} years old`);
-console.log(`You are going to be ${age+10} years old in 10 years.`);
-console.log(`You are going to be ${age+20} years old in 20 years.`);
+
+for (let future = 10; future <= 40; future += 10) {
+    console.log(`You are ${age + future} years old in ${future} years. `);
+}
+//console.log(`You are going to be ${age+10} years old in 10 years.`);
+//console.log(`You are going to be ${age+20} years old in 20 years.`);
 
 
---
+++ factorial.js
@@ -0,0 +1,16 @@
+//Write a function that computes and returns the factorial of a number 
+//by using a for loop. Use a loop of some kind to solve this
+
+function factorial(number) {
+    let result = 1;
+    for (let counter = number; counter > 0; counter -= 1) {
+      result *= counter;
+    }
+  
+    return result;
+  }
+
+  console.log(factorial(0));     // => 1
+  console.log(factorial(1));     // => 1
+  console.log(factorial(2));     // => 2
+  console.log(factorial(100));     // => 6
